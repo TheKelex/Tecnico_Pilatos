32e2f3293e4f163c14b331ef57d809c0ed8fb5e3
TheKelex <ksaavedrachantris@gmail.com>
2025-05-28 11:43:04 -0500

    Envio De Modal Nuevo

---
+++ nuevo/insertar_pedido.php
@@ -0,0 +1,27 @@
+<?php
+
+    include("conexion.php");
+
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $nombre = $_POST["nombre"];
+    $direccion = $_POST["direccion"];
+    $ciudad = $_POST["ciudad"];
+    $departamento = $_POST["departamento"];
+    $codigopostal = $_POST["codigo_postal"];
+
+    if ($email && $password && $nombre && $direccion && $ciudad && $departamento && $codigopostal) {
+
+        $sql = "INSERT INTO pedido (id_pedido, email_pedido, password_pedido, nombre_pedido, direccion_pedido, ciudad_pedido, departamento_pedido, codigo_postal_pedido, fecha_pedido) VALUES (NULL, '$email', '$password', '$nombre', '$direccion', '$ciudad', '$departamento', '$codigopostal', NULL)";
+        mysqli_query($mysqli, $sql);
+
+        echo '<script type="text/javascript"> alert("Tarea Guardada");
+        window.location.href = "./nuevo.html"; </script>';
+
+    } else {
+
+        echo "Debes llenar todos los campos";
+
+    }
+
+?>
